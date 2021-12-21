5b6369fd588ba57c08d6e9e9f71dc9cffd5f9cb6
Ayobami <64878932+Ayovert@users.noreply.github.com>
2021-12-21 09:39:29 +0100

    Addit

--- src/components/CardComp.js
+++ src/components/CardComp.js
@@ -1,17 +1,19 @@
 import React from 'react';
 import { Breadcrumb, BreadcrumbItem, Card, CardBody, CardImg, CardTitle, CardText, CardLink, CardHeader, Media, Button, ListGroup, ListGroupItem } from 'reactstrap';
-import { Link } from 'react-router-dom';
-import { Loading } from './LoadingComponent';
+
 import { baseUrl } from '../shared/baseUrl';
-import { Fade, Stagger } from 'react-animation-components';
+
+
+
+
 
 
 function CardComp() {
     return (
         <div className="container">
-          
+
             <div className="row row-content">
-               
+
                 <div className="col-12 col-md-5">
                     <Card>
                         <CardHeader className="bg-primary text-white">Facts At a Glance</CardHeader>
@@ -32,26 +34,57 @@ function CardComp() {
             </div >
 
             <div className="row row-content">
-            <div className="col-12 col-md-5">
-                <Card style={{ width: '18rem' }}>
-                    <CardImg variant="top" src={baseUrl + 'images/vadonut.png'} className="p-1"/>
-                    <CardBody>
+                <div className="col-12 col-md-5">
+                    <Card>
+                        <CardImg variant="top" src={baseUrl + 'images/vadonut.png'} className="p-1"
+                        />
+                        <CardBody>
 
-                        <div style={{ display: 'flex', alignContent: 'center', justifyContent: 'center', marginBottom: 5 }}>
-                            <img className=".img-responsive" alt="" src={baseUrl + 'images/vadonut.png'} width={80} height={80} />
-                        </div>
-                    </CardBody>
-                    <CardBody>
-                        <div style={{ display: 'flex', alignContent: 'center', justifyContent: 'center', marginBottom: 5 }}>
-                            <Button variant="primary">Go somewhere</Button>
-                        </div>
-                    </CardBody>
-                </Card>
+                            <div style={{ display: 'flex', alignContent: 'center', justifyContent: 'center', marginBottom: 5 }}>
+                                <img className=".img-responsive" alt="" src={baseUrl + 'images/vadonut.png'} width={80} height={80} />
+                            </div>
+                        </CardBody>
+                        <CardBody>
+                            <div style={{ display: 'flex', alignContent: 'center', justifyContent: 'center', marginBottom: 5 }}>
+                                <Button variant="primary">Go somewhere</Button>
+                            </div>
+                        </CardBody>
+                    </Card>
 
+                </div>
             </div>
-            </div>
+
+
+            <div class="row">
+    <div class="col-xs-6 col-md-4">
+      <div class="card">
+        <div class="wrapper">
+          <img class="card-img-top img-fluid" src="//placehold.it/760x270" alt="Card image cap" alt=""/>
+        </div>
+        <h4 class="card-title">Title</h4>
+        <div class="card-body">
+          <p class="card-text">Some quick example text to build on </p>
+          10 mins ago <div class="float-right"><i class="fa fa-comment-o" aria-hidden="true"></i> 0</div>
+        </div>
+      </div>
+    </div>
+    <div class="col-xs-6 col-md-4">
+      <div class="card">
+        <div class="wrapper">
+          <img class="card-img-top img-fluid" src="//placehold.it/760x500" alt="Card image cap" alt= ""/>
+        </div>
+        <h4 class="card-title">Title</h4>
+        <div class="card-body">
+          <p class="card-text">Some quick example text to build on </p>
+          10 mins ago <div class="float-right"><i class="fa fa-comment-o" aria-hidden="true"></i> 0</div>
+        </div>
+      </div>
+    </div>
+  </div>
+
         </div>
     );
 }
 
 export default CardComp;
+
