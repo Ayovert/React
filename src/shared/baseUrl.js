b33c4dd3a3f46b5a184a6f54a015f2e49e53c5e0
Ayobami Jimoh <haryorbumz@gmail.com>
2022-03-29 00:57:55 +0100

    add json server

--- src/shared/baseUrl.js
+++ src/shared/baseUrl.js
@@ -1,2 +1,2 @@
-export const baseUrl = 'http://localhost:3001/';
+export const baseUrl = 'https://ay-json-server.herokuapp.com/';
 export const walletUrl = 'https://api.wallets.africa/cards/create';
